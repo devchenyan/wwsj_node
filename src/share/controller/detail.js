d5409e6f33f06229d38cf67f39fe53c6fba19857
chenyan26 <sgt39007@163.com>
2017-05-09 11:01:29 +0800

    同步

--- src/share/controller/detail.js
+++ src/share/controller/detail.js
@@ -40,6 +40,30 @@ export default class extends Base {
     return this.display();
   }
 
+  // 求租信息
+  async rentAction(){
+    let instance = this.model('cms_buyer');
+    let obj = await instance.where({id: this.get('id')}).find();
+    let user = obj.user;
+
+    this.assign({
+      avatarPath: this.getAvatarUrl(user.avatar.path),
+      nickname: user.nickname,
+
+      addtime: this.getAddtime(obj.addtime),
+      description: obj.description,
+
+      brand: obj.brand,
+      cartype: obj.cartype,
+      version: obj.version,
+
+      contacts: obj.contacts,
+      phone: this.getPhoneNum(obj.phone),// + "下载App查看",
+      address: obj.address,
+    });
+    return this.display();
+  }
+
   // 卖车信息
   async sellAction(){
     let instance = this.model('cms_seller');
@@ -54,7 +78,49 @@ export default class extends Base {
     if (images.length) {
       for (let i = 0; i < images.length; i++) {
         let upload = await uploadInstance.where({id: images[i]}).find();
-        imageUrls.push("http://eswjdg.com/" + upload.path);
+        imageUrls.push(urlPrefix + upload.path);
+      }
+    }
+
+    this.assign({
+      avatarPath: this.getAvatarUrl(user.avatar.path),
+      nickname: user.nickname,
+
+      addtime: this.getAddtime(obj.addtime),
+      description: obj.description,
+
+      brand: obj.brand,
+      cartype: obj.cartype,
+      version: obj.version,
+
+      price: obj.price,
+      madetime: obj.madetime,
+      worktime: obj.worktime,
+
+      contacts: obj.contacts,
+      phone: this.getPhoneNum(obj.phone),// + "下载App查看",
+      address: obj.address,
+
+      imageUrls: imageUrls
+    });
+    return this.display();
+  }
+
+  // 出租信息
+  async leaseAction(){
+    let instance = this.model('cms_seller');
+    let obj = await instance.where({id: this.get('id')}).find();
+    let user = obj.user;
+
+    let images = obj.images.split(",");
+    let imageUrls = [];
+
+    let uploadInstance = this.model('admin_upload');
+
+    if (images.length) {
+      for (let i = 0; i < images.length; i++) {
+        let upload = await uploadInstance.where({id: images[i]}).find();
+        imageUrls.push(urlPrefix + upload.path);
       }
     }
 
